1710f6be4470d28a09d58e3fc481a3582f8e12e6
Sebastian Lucker <seblucker@gmail.com>
2023-12-08 22:07:41 +0000

    Week 1 Commit 4
    More UI
    - Timer works as intended
    - UI Manager

--- Assets/Sebastian/TimerController.cs
+++ Assets/Sebastian/TimerController.cs
@@ -6,28 +6,79 @@ using TMPro;
 
 public class TimerController : MonoBehaviour
 {
-    [SerializeField] private float timeCounter;
-    [SerializeField] private float countdownTimer = 120f;
-    [SerializeField] private int minutes;
-    [SerializeField] private int seconds;
-    [SerializeField] private bool isCountdown;
+    [SerializeField] private float updateTime = 1f;
     [SerializeField] private TextMeshProUGUI timerText;
-    [SerializeField] private int TimerMultiplier;
 
-    private void Update()
-    {           // Countdown Timer
-        if (isCountdown && countdownTimer > 0) { 
-            countdownTimer -= Time.deltaTime;
-            minutes = Mathf.FloorToInt(countdownTimer / 60f);
-            seconds = Mathf.FloorToInt(countdownTimer -  minutes * 60);
-            } 
-                else if (!isCountdown)
-            {       // Stopwatch like timer
-                timeCounter += (Time.deltaTime * TimerMultiplier);
-                minutes = Mathf.FloorToInt(timeCounter / 60f);
-                seconds = Mathf.FloorToInt(timeCounter - minutes * 60);
+    [SerializeField] private int dayEndHour = 17;
+    [SerializeField] private int dayEndMinute = 0;
+
+    private int hours;
+    private int minutes;
+
+    public delegate void TimerHit();
+
+    public event TimerHit OnDayEnd;
+
+    private void Start()
+    {
+        StartCounter(09, 00);
+    }
+
+    bool isCounting = false;
+    Coroutine C_Counting;
+
+    private void StartCounter(int startingHour, int startingMinute)
+    {
+        if (isCounting) return;
+
+        isCounting = true;
+
+        if (C_Counting != null) return;
+
+        C_Counting = StartCoroutine(IncrementTime(startingHour, startingMinute));
+    }
+
+    private void EndCounting()
+    {
+        if (!isCounting) return;
+
+        isCounting = false;
+
+        if (C_Counting == null) return;
+
+        StopCoroutine(C_Counting);
+    }
+
+    private IEnumerator IncrementTime(int startingHour, int startingMinute)
+    {
+        hours = startingHour;
+        minutes = startingMinute;
+
+        while (isCounting)
+        {
+            minutes++;
+
+            if (minutes >= 60)
+            {
+                hours++;
+
+                if(hours >= 24)
+                {
+                    hours = 0;
+                }
+
+                minutes = 0;
             }
-        
-        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+
+            timerText.text = string.Format("{0:00}:{1:00}", hours, minutes);
+
+            if(hours == dayEndHour && minutes == dayEndMinute)
+            {
+                OnDayEnd?.Invoke();
+                break;
+            }
+
+            yield return new WaitForSeconds(updateTime);
+        }
     }
 }
