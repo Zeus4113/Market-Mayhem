d277079adac56009648770eb4874ea4bb3903732
Sebastian Lucker <seblucker@gmail.com>
2023-12-10 18:26:37 +0000

    Week 1 Commit 5
    Start of Loading Screen

---
+++ Assets/Sebastian/LoadingScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] string levelName;
+
+    private IEnumerator LoadNextLevel(string levelName)
+    {
+        AsyncOperation loadlevel = SceneManager.LoadSceneAsync(levelName);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+}
