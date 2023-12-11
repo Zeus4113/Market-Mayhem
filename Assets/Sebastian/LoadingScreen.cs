8d6f4b8c34f9988d68e80c5d39079fdb95828fa6
Sebastian Lucker <seblucker@gmail.com>
2023-12-11 00:28:50 +0000

    Week 2 Commit 1
    Loading Screen 'Working'

--- Assets/Sebastian/LoadingScreen.cs
+++ Assets/Sebastian/LoadingScreen.cs
@@ -7,17 +7,67 @@ using UnityEngine.SceneManagement;
 
 public class LoadingScreen : MonoBehaviour
 {
-    [SerializeField] string levelName;
+    [SerializeField] private string[] desiredLevels;
 
-    private IEnumerator LoadNextLevel(string levelName)
+    private Vector2 m_startPosition;
+    private Vector2 m_endPosition;
+
+    private Image m_loadingScreenImage;
+    private float m_loadingScreenSpeed;
+    private Vector2 m_loadingScreenPositon;
+    private float m_duration = 5f;
+
+
+    public void LoadLevelScreen()
     {
-        AsyncOperation loadlevel = SceneManager.LoadSceneAsync(levelName);
-
-		yield return null;
+        StartCoroutine(DrawLoadingScreen(desiredLevels));
     }
 
-    private void Start()
+
+
+
+    private IEnumerator DrawLoadingScreen(string[] desiredLevels)
     {
-       // StartCoroutine(LoadNextLevel());
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LoadingScreen");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+
+        float time = 0;
+
+        while (time < m_duration)
+        {
+            time += Time.fixedDeltaTime;
+            m_loadingScreenImage.transform.position = Vector2.Lerp(m_startPosition, m_endPosition, time / m_duration);
+            yield return new WaitForFixedUpdate();
+
+        }
+
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene myScene = SceneManager.GetSceneAt(i);
+            if (myScene.name != "LoadingScreen") UnloadLevels(myScene);
+        }
+
+        for(int i = 0; i < desiredLevels.Length; i++)
+        {
+            LoadLevels(desiredLevels[i]);
+        }
+
+        Scene loadingScene = SceneManager.GetSceneByName("LoadingScreen");
+        UnloadLevels(loadingScene);
+
+    }
+
+    private void UnloadLevels(Scene scene)
+    {
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+    private void LoadLevels(string scene)
+    {
+        SceneManager.LoadSceneAsync(scene);
     }
 }
